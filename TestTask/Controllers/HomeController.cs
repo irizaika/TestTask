47663b16f8c85583d64fd55f024f2c6cf54c694e
Irina <irikuszaika@gmail.com>
2026-06-04 23:33:55 +0300

    Date type corrected

--- TestTask/Controllers/HomeController.cs
+++ TestTask/Controllers/HomeController.cs
@@ -16,24 +16,24 @@ namespace TestTask.Controllers
 
                 Days =
                 [
-                    new() { Date = "2026-06-01", Positions = 2, Pnl = 120, PNLPercent = 1.2 },
-                    new() { Date = "2026-06-02", Positions = 0, Pnl = 0, PNLPercent = 0 },
-                    new() { Date = "2026-06-03", Positions = 1, Pnl = -50, PNLPercent = -0.5 },
-                    new() { Date = "2026-06-04", Positions = 3, Pnl = 300, PNLPercent = 2.1 },
-                    new() { Date = "2026-06-05", Positions = 2, Pnl = 80, PNLPercent = 0.8 },
-                    new() { Date = "2026-06-07", Positions = 4, Pnl = -200, PNLPercent = -1.5 },
-                    new() { Date = "2026-06-08", Positions = 1, Pnl = 40, PNLPercent = 0.3 },
-                    new() { Date = "2026-06-09", Positions = 0, Pnl = 0, PNLPercent = 0 },
-                    new() { Date = "2026-06-10", Positions = 2, Pnl = 150, PNLPercent = 1.1 },
-                    new() { Date = "2026-06-15", Positions = 1, Pnl = 4, PNLPercent = 0.35 },
-                    new() { Date = "2026-06-16", Positions = 0, Pnl = 0, PNLPercent = 0 },
-                        new() { Date = "2026-06-17", Positions = 2, Pnl = 10, PNLPercent = 33 },
-                        new() { Date = "2026-06-22", Positions = 1, Pnl = 4, PNLPercent = 0.35 },
-                        new() { Date = "2026-06-23", Positions = 0, Pnl = 0, PNLPercent = 0 },
-                        new() { Date = "2026-06-24", Positions = 2, Pnl = 10, PNLPercent = 33 },
-                        new() { Date = "2026-06-28", Positions = 0},
-                        new() { Date = "2026-06-29", Positions = 1, Pnl = 4, PNLPercent = 0.35 },
-                        new() { Date = "2026-06-30", Positions = 0, Pnl = 0, PNLPercent = 0 }
+                    new() { Date = new DateOnly(2026, 06, 01), Positions = 2, Pnl = 120, PNLPercent = 1.2 },
+                    new() { Date = new DateOnly(2026, 06, 02), Positions = 0, Pnl = 0, PNLPercent = 0 },
+                    new() { Date = new DateOnly(2026, 06, 03), Positions = 1, Pnl = -50, PNLPercent = -0.5 },
+                    new() { Date = new DateOnly(2026, 06, 04), Positions = 3, Pnl = 300, PNLPercent = 2.1 },
+                    new() { Date = new DateOnly(2026, 06, 05), Positions = 2, Pnl = 80, PNLPercent = 0.8 },
+                    new() { Date = new DateOnly(2026, 06, 07), Positions = 4, Pnl = -200, PNLPercent = -1.5 },
+                    new() { Date = new DateOnly(2026, 06, 08), Positions = 1, Pnl = 40, PNLPercent = 0.3 },
+                    new() { Date = new DateOnly(2026, 06, 09), Positions = 0, Pnl = 0, PNLPercent = 0 },
+                    new() { Date = new DateOnly(2026, 06, 10), Positions = 2, Pnl = 150, PNLPercent = 1.1 },
+                    new() { Date = new DateOnly(2026, 06, 15), Positions = 1, Pnl = 4, PNLPercent = 0.35 },
+                    new() { Date = new DateOnly(2026, 06, 16), Positions = 0, Pnl = 0, PNLPercent = 0 },
+                    new() { Date = new DateOnly(2026, 06, 17), Positions = 2, Pnl = 10, PNLPercent = 33 },
+                    new() { Date = new DateOnly(2026, 06, 22), Positions = 1, Pnl = 4, PNLPercent = 0.35 },
+                    new() { Date = new DateOnly(2026, 06, 23), Positions = 0, Pnl = 0, PNLPercent = 0 },
+                    new() { Date = new DateOnly(2026, 06, 24), Positions = 2, Pnl = 10, PNLPercent = 33 },
+                    new() { Date = new DateOnly(2026, 06, 28), Positions = 0, Pnl = 0, PNLPercent = 0 },
+                    new() { Date = new DateOnly(2026, 06, 29), Positions = 1, Pnl = 4, PNLPercent = 0.35 },
+                    new() { Date = new DateOnly(2026, 06, 30), Positions = 0, Pnl = 0, PNLPercent = 0 }
                 ]
 
             };
--- TestTask/Models/CalendarDay.cs
+++ TestTask/Models/CalendarDay.cs
@@ -2,7 +2,7 @@
 {
     public class CalendarDay
     {
-        public required string Date { get; set; }
+        public DateOnly Date { get; set; }
         public int? Positions { get; set; }
         public double? PNLPercent { get; set; }
         public double? Pnl { get; set; }
